5c17967b1da6328665bb82ba21dfedbace6d426c
mdsjjorge <mdsousa.jorge@gmail.com>
2022-08-18 09:59:59 -0300

    adc cod buscador + funcao searchClean

--- recipeList.js
+++ recipeList.js
@@ -53,55 +53,40 @@ let recipe = [
         ]
     },
     {
-        name: 'Maminha no Alho',
+        name: 'Espetinhos Árabes',
         ingredients: [
-             '1 peça de maminha (cerca de 1 e meio quilo)',
+             '500 g de carne moída',
+             '3 colheres (sopa) de hortelã',
+             '1 cebola picada',
+             '2 dentes de alho amassados',
+             '3 colheres (sopa) de salsa picada',
+             '1 colher (sopa) de mostarda',
+             '1 colher (sopa) de molho inglês',
+             '1 pitada de pimenta-do-reino moída',
+             '1 colher (sopa) de MAGGI® Gril',
+             'margarina light para pincelar',
+             'suco de 2 laranjas'
+        ],
+        method: [
+            'Em um recipiente, misture a carne com a hortelã, a cebola, o alho, a salsa, a mostarda, o molho inglês, a pimenta e o MAGGI Gril.',
+            'Modele em palitos para churrasco, apertando com as mãos para formar os espetinhos.',            
+            'Aqueça uma frigideira grande e pincele com a margarina. Grelhe os espetinhos virando e pincelando com o suco de laranja até dourar.'
+        ]
+    },
+    {
+        name: 'Pão de Alho',
+        ingredients: [
+             '4 baguetes médias cerca de 100 g cada',
              '100 g de manteiga',
-             '1 e meia colher (sopa) de MAGGI® Gril',
-             '1 cabeça de alho'
+             '3 dentes de alho amassados',
+             'meia colher (sopa) de MAGGI® Fondor',
+             '2 colheres (sopa) de cheiro-verde picado',
+             '2 colheres (sopa) de queijo parmesão ralado'
         ],
         method: [
-            'Com o auxílio de uma faca, fure a peça de MAMINHA.',
-            'Em um recipiente, misture a manteiga com o MAGGI Gril e os dentes de alho amassados e besunte toda a carne, principalmente entre os furos.',
-            'Embrulhe-a em papel-alumínio e asse-a na churrasqueira de acordo com o ponto desejado da carne.',
-            'Corte a carne em fatias e sirva a seguir.'
+            'Corte as baguetes em fatias, sem separá-las da base do pão e reserve.',
+            'Em um recipiente misture bem a manteiga, o alho, o MAGGI Fondor, o cheiro verde e o queijo parmesão, formando uma pasta cremosa.',           
+            'Recheie com esta pasta os espaços entre as fatias de pão e leve-os para assar em forno médio (180°C), preaquecido, até dourar.'
         ]
     },
-    {
-        name: 'Picanha na Grelha',
-        ingredients: [
-            '1 e meia xícara (chá) de shitake cerca de 200 g',
-            '1 picanha pequena cerca de 1,2 kg',
-            '1 colher (sopa) de MAGGI® Gril',
-            '1 e meia xícara (chá) de mini cebolas cerca de 200 g',
-            '1 e meia xícara (chá) de batatas bolinhas cozidas cerca de 200 g',
-            '1 abobrinha em rodelas'
-        ],
-        method: [
-            'Retire os cabinhos dos shitakes.',
-            'Coloque-os de molho em água por cerca de 20 minutos.',
-            'Escorra a água, pique e reserve.',
-            'Em uma grelha, doure a picanha e polvilhe o MAGGI Gril.',
-            'Vire-a várias vezes, até atingir o ponto desejado.',
-            'Na mesma grelha, doure o shitake, as mini cebolas, as batatas e a abobrinha.',
-            'Sirva acompanhado da picanha.'
-        ]
-    },
-    {
-        name: 'Farofa de Linguiça',
-        ingredients: [
-            '2 colheres (sopa) de manteiga',
-            '1 cebola ralada',
-            '100 g de linguiça fresca',
-            '1 xícara (chá) de farinha de mandioca crua',
-            'meia colher (sopa) de MAGGI® Gril',
-            'meia xícara (chá) de azeitonas verdes picadas',
-            '1 ovo cozido e picado',
-            'meia xícara (chá) de salsa picada'
-        ],
-        method: [
-            'Em uma panela, aqueça a manteiga e doure a cebola. Acrescente a linguiça e refogue bem.',
-            'Junte a farinha de mandioca, o MAGGI Gril, as azeitonas e o ovo cozido, mexendo bem, sem deixar grudar no fundo da panela. Finalize com a salsa e sirva.'
-        ]
-    }
 ]
